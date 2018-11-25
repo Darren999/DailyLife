6052d79b9ccf28abea88517b00e1b577fbc06bfa
Darren.fan <2511132466@qq.com>
2018-11-25 15:48:07 +0800

    add aboutmeActivity

--- app/src/main/java/view/darren/com/dailylife/ui/guide/view/activity/AboutMeActivity.java
+++ app/src/main/java/view/darren/com/dailylife/ui/guide/view/activity/AboutMeActivity.java
@@ -1,17 +1,24 @@
 package view.darren.com.dailylife.ui.guide.view.activity;
 
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import butterknife.Bind;
 import view.darren.com.dailylife.R;
 import view.darren.com.dailylife.base.mvp.BaseActivity;
 
 public class AboutMeActivity extends BaseActivity implements View.OnClickListener{
 
-
-    @Override
-    public void onClick(View view) {
-
-    }
-
+    @Bind(R.id.ll_title_menu)
+    FrameLayout llTitleMenu;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.toolbar_title)
+    TextView toolbarTitle;
     @Override
     public int getContentView() {
         return R.layout.activity_about_me;
@@ -19,16 +26,37 @@ public class AboutMeActivity extends BaseActivity implements View.OnClickListene
 
     @Override
     public void initView() {
+        initToolBar();
+    }
 
+    private void initToolBar() {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
+
+        toolbarTitle.setText("关于我");
     }
 
     @Override
     public void initListener() {
-
+        llTitleMenu.setOnClickListener(this);
     }
 
     @Override
     public void initData() {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.ll_title_menu:
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 }
